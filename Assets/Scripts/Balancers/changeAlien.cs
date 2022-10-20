ca6f884042ac14bb375561993ef9df10ca12b377
seymourbutttz <mseymour0307@gmail.com>
2022-10-20 15:58:55 -0700

    updated cheat sheet
    allows spell cheats, and tall alien cheats

---
+++ Assets/Scripts/Balancers/changeAlien.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeAlien : MonoBehaviour
+{
+    //script to change stats of the regular sized alien
+
+
+}
