6d455e44a7d7e4e25aa8506811644eb08a0983fc
seymourbutttz <mseymour0307@gmail.com>
2022-10-25 00:00:24 -0700

    shock tower testing

--- Assets/Scripts/Towers/ShockTower.cs
+++ Assets/Scripts/Towers/ShockTower.cs
@@ -20,7 +20,13 @@ public class ShockTower : MonoBehaviour
     public GameObject shockEffect; //visual effect for shocking enemy
     private Transform target; //target location
     private EnemyController targetEnemy; //target enemy
+    private EnemyController enemy2, enemy3; //additional enemies
 
+    //[HideInInspector]
+    //public List<EnemyController> enemiesInRange = new List<EnemyController>(); //list of enemies within range
+
+    public float chainDamage1; //damage ratio for single chain
+    public float chainDamage2; //damage ratio for second chain
 
     // Start is called before the first frame update
     void Start()
@@ -56,9 +62,9 @@ public class ShockTower : MonoBehaviour
             else
             {
                 target = null;
+                targetEnemy = null;
             }
         }
-
         damageEnemy(); //damages enemy for dps
     }
 
@@ -68,9 +74,47 @@ public class ShockTower : MonoBehaviour
         //if statement rotating source of effect
         if (target != null)
         {
-            source.rotation = Quaternion.Slerp(source.rotation, Quaternion.LookRotation(target.position - transform.position), 5f * Time.deltaTime);
-            source.rotation = Quaternion.Euler(0f, source.rotation.eulerAngles.y, 0f);
-            targetEnemy.GetComponent<EnemyHealthController>().TakeDamage(DPS * Time.deltaTime); //attacks enemy
+            if(theTower.GetComponent<TowerUpgradeController>().currentTowerUpgrade == 0) //attacks a single enemy
+            {
+                SingleEnemyAttack();
+            }else if (theTower.GetComponent<TowerUpgradeController>().currentTowerUpgrade == 1) //attacks closest enemy and next two closest enemies at reduced damage
+            {
+                SingleEnemyAttack();
+                //ChainAttack();
+                
+            }
+            else if (theTower.GetComponent<TowerUpgradeController>().currentTowerUpgrade == 2) //attacks closest enemy and next four closest enemies at reduced damage
+            {
+                SingleEnemyAttack();
+                //ChainAttack();
+                //SecondChainAttack();
+            }
+            
+        }
+    }
+
+    //attacks single enemy
+    public void SingleEnemyAttack()
+    {
+        source.rotation = Quaternion.Slerp(source.rotation, Quaternion.LookRotation(target.position - transform.position), 5f * Time.deltaTime); //rotates towards enemy
+        source.rotation = Quaternion.Euler(0f, source.rotation.eulerAngles.y, 0f); //slowly rotates doesnt snap
+        targetEnemy.GetComponent<EnemyHealthController>().TakeDamage(DPS * Time.deltaTime); //attacks enemy
+    }
+
+    //deals damage to enemies attached to first chain of tower
+    public void ChainAttack()
+    {
+        enemy2.GetComponent<EnemyHealthController>().TakeDamage(DPS * chainDamage1 * Time.deltaTime);
+        Debug.Log(theTower.enemiesInRange.Count);
+        Debug.Log(targetEnemy.GetComponent<EnemyHealthController>().totalHealth + ", " + enemy2.GetComponent<EnemyHealthController>().totalHealth);
+    }
+
+    //deals damage to enemies attached to the second chain of tower
+    public void SecondChainAttack()
+    {
+        if (theTower.enemiesInRange.Count > 3)
+        {
+            enemy3.GetComponent<EnemyHealthController>().TakeDamage(DPS * chainDamage2 * Time.deltaTime);
         }
     }
 }
